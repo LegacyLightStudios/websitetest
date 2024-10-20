afa37574eae4849a084b4c72a693b64814f4f283
LegcyLightStudios <LegcyLightStudios@users.noreply.github.com>
2024-10-21 00:24:59 +0200

    com

--- script.js
+++
@@ -1,6 +0,0 @@
-// Change Text Functionality
-document.getElementById("changeTextBtn").addEventListener("click", function() {
-    const dynamicText = document.getElementById("dynamicText");
-    dynamicText.textContent = "The text has been changed! JavaScript is awesome!";
-});
-
